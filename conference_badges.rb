4f9611d29448415a5f761b832eea5c1f363335f4
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 01:46:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_array = []
+  array.each do |i|
+    badge_array << "Hello, my name is #{i}."
+  end 
+  badge_array
+end
+
+def assign_rooms(array)
+  room_number = []
+  room_welcome = []
+  array.each do |i|
+    room_welcome << "Hello, #{i}! You'll be assigned to room "
+end
