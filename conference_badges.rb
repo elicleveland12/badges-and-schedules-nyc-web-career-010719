f9a5fce1ac4d0bb26ae769fe6cc68da3cd185401
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 01:48:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,8 @@ def assign_rooms(array)
   room_number = []
   room_welcome = []
   array.each_with_index { |item, index| 
-  
+    array[item] = index
+    
   array.each do |n|
     room_welcome << "Hello, #{n}! You'll be assigned to room "
 end
